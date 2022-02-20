e3ea76f070d09aa1a72e9ac187d485f1b6f187e0
duongdaiphuoc0501 <phuocrider25@gmail.com>
2022-02-20 11:03:49 +0700

    bt DT connect

---
+++ connect_backend/thuc_hanh/ung_dung_blog/blog/src/app/post.ts
@@ -0,0 +1,13 @@
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export class IPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
